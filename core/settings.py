516ed45c683fb5dbf919d2d39f4a79009208533b
AhmedSaied94 <ahmed.saeed311294@gmail.com>
2022-06-24 03:03:43 +0200

    close debug

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ load_dotenv(find_dotenv())
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 CORS_ALLOW_ALL_ORIGINS = True
 ALLOWED_HOSTS = ['*']
 
